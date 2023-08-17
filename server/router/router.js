4ac8103e6d4855a04fa2db5896e88a47db0b3f4b
Alihan <82150661+alienslimeson@users.noreply.github.com>
2023-08-17 15:13:40 +0300

    Modals and Routes

--- server/router/router.js
+++ server/router/router.js
@@ -1,23 +1,78 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 import Post from '../Models/Post.js';
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  
+  try {
+    const posts = await Post.find();
+
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await Post.findById(id);
+
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 });
 
 router.post('/', async (req, res) => {
- 
+  const post = req.body;
+
+  const newPost = new Post(post);
+
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 });
 
 router.put('/:id', async (req, res) => {
- 
+  try {
+    const { id } = req.params;
+    const { title, content, creator } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return res.status(404).send(`No post with id: ${id}`);
+
+    const updatedPost = { title, content, creator, _id: id };
+
+    await Post.findByIdAndUpdate(id, updatedPost, { new: true });
+
+    res.json(updatedPost);
+  } catch (error) {
+    console.log(error);
+    0;
+  }
 });
 
 router.delete('/:id', async (req, res) => {
- 
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return res.status(404).send(`No post with id: ${id}`);
+
+    await Post.findByIdAndRemove(id);
+
+    res.json({ message: 'Post deleted successfully.' });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 export default router;
